89b92f739f8fdb055e0c57d6d7f68a6b623b8363
flash <flash.lin@gmail.com>
2017-11-03 17:39:06 +0800

    test

---
+++ KataPokerTest/PokerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace KataPokerTest
+{
+    [TestFixture]
+    public class PokerTest
+    {
+        [TestCase("2H 3D 5S 9C KD", "2C 3H 4S 8C AH", "White wins. - with high card: Ace")]
+        [Test]
+        public void High_Card(string blackCards, string whiteCards, string expected)
+        {
+            var game = new PokerGame();
+            var gameResult = game.Input(blackCards, whiteCards);
+            Assert.AreEqual(expected, gameResult);
+        }
+
+        [Test]
+        public void Pair()
+        {
+            var game = new PokerGame();
+            var gameResult = game.Input("2H 2D 5S 9C KD", "2C 3H 4S 8C AH");
+            Assert.AreEqual("Black wins. - with pair card: 2H 2D", gameResult);
+        }
+    }
+
+    public class PokerGame
+    {
+        public string Input(string blackCards, string whiteCards)
+        {
+            return "White wins. - with high card: Ace";
+        }
+    }
+}
---
+++ KataPokerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KataPokerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KataPokerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("446abec5-bb99-44fc-9ac1-4520a4f51d5b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
